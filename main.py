0abd9e980b092506b61f40ac3535f725daf99863
KingVishal703 <vishal21wankhede@gmail.com>
2025-12-31 09:02:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Bot Token (Environment variable or default fallback)
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7648577586:AAG10G2khDJyFiQtwhVT7fyhjjo_AX8jFeI")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7614452604:AAE0P2nRaJzsmwFFi5mvxVZ-InowwEqfsww")
 
 # --- Commands ---
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
